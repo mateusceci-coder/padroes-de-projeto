6d061bf7d5ce22dca08ada83b6b410ea3358ddf3
EmersonLaranja <emersonlaranja@gmail.com>
2024-03-12 15:06:17 -0300

    1.2

--- src/adapters/controllers/task/addTask.ts
+++ src/adapters/controllers/task/addTask.ts
@@ -7,17 +7,18 @@ import {
   created,
 } from "../../presentations/api/httpResponses/httpResponses";
 import { Request, Response } from "express";
+import { Controller } from "../../interfaces/controller";
 
-export class AddTaskController {
-  async handle(req: Request, res: Response) {
+export class AddTaskController implements Controller {
+  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     const requiredFields = ["title", "description", "date"];
 
     for (const field of requiredFields) {
-      if (!req.body[field]) {
+      if (!httpRequest.body[field]) {
         return badRequest(new MissingParamError(field));
       }
     }
-    const { title, description, date } = req.body;
+    const { title, description, date } = httpRequest.body;
 
     const isValid = validator.isDate(date, {
       format: "DD-MM-YYYY",
@@ -28,6 +29,6 @@ export class AddTaskController {
     }
 
     const task = { title, description, date };
-    res.json(created(task));
+    return created(task);
   }
 }
