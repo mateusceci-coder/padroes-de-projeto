d9c1ea96c2a9fe7ad3845ef30c5cb1382e585b1d
EmersonLaranja <emersonlaranja@gmail.com>
2024-03-12 16:23:12 -0300

    1.4

--- src/adapters/controllers/task/addTask.ts
+++ src/adapters/controllers/task/addTask.ts
@@ -8,8 +8,10 @@ import {
 } from "../../presentations/api/httpResponses/httpResponses";
 import { Request, Response } from "express";
 import { Controller } from "../../interfaces/controller";
+import { DateValidator } from "../../interfaces/dateValidator";
 
 export class AddTaskController implements Controller {
+  constructor(private readonly dateValidator: DateValidator) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     const requiredFields = ["title", "description", "date"];
 
@@ -20,9 +22,7 @@ export class AddTaskController implements Controller {
     }
     const { title, description, date } = httpRequest.body;
 
-    const isValid = validator.isDate(date, {
-      format: "DD-MM-YYYY",
-    });
+    const isValid = this.dateValidator.isValid(date);
 
     if (!isValid) {
       return badRequest(new InvalidParamError("date"));
