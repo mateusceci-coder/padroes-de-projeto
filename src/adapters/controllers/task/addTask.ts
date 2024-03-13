035f207de725df7047278d06068c5b324cd89435
EmersonLaranja <emersonlaranja@gmail.com>
2024-03-13 14:23:07 -0300

    3.1

--- src/adapters/controllers/task/addTask.ts
+++ src/adapters/controllers/task/addTask.ts
@@ -1,15 +1,14 @@
-import validator from "validator";
+import { AddTask } from "../../../usecases/addTask";
+import { Controller } from "../../interfaces/controller";
+import { DateValidator } from "../../interfaces/dateValidator";
 import { HttpRequest, HttpResponse } from "../../interfaces/http";
 import { InvalidParamError } from "../../presentations/api/errors/invalid-param-error";
 import { MissingParamError } from "../../presentations/api/errors/missing-param-error";
 import {
   badRequest,
   created,
+  serverError,
 } from "../../presentations/api/httpResponses/httpResponses";
-import { Request, Response } from "express";
-import { Controller } from "../../interfaces/controller";
-import { DateValidator } from "../../interfaces/dateValidator";
-import { AddTask } from "../../../usecases/addTask";
 
 export class AddTaskController implements Controller {
   constructor(
@@ -17,22 +16,27 @@ export class AddTaskController implements Controller {
     private readonly dateValidator: DateValidator
   ) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const requiredFields = ["title", "description", "date"];
+    try {
+      const requiredFields = ["title", "description", "date"];
 
-    for (const field of requiredFields) {
-      if (!httpRequest.body[field]) {
-        return badRequest(new MissingParamError(field));
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field));
+        }
       }
+      const { title, description, date } = httpRequest.body;
+
+      const isValid = this.dateValidator.isValid(date);
+
+      if (!isValid) {
+        return badRequest(new InvalidParamError("date"));
+      }
+
+      const task = await this.addTask.add({ title, description, date });
+      // return created(task);
+      throw new Error("Forced error");
+    } catch (error: any) {
+      return serverError(error);
     }
-    const { title, description, date } = httpRequest.body;
-
-    const isValid = this.dateValidator.isValid(date);
-
-    if (!isValid) {
-      return badRequest(new InvalidParamError("date"));
-    }
-
-    const task = await this.addTask.add({ title, description, date });
-    return created(task);
   }
 }
