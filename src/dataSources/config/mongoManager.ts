5a19cb128892f62a4356b667816add1285951641
EmersonLaranja <emersonlaranja@gmail.com>
2024-03-13 09:24:05 -0300

    2.2

--- src/dataSources/config/mongoManager.ts
+++ src/dataSources/config/mongoManager.ts
@@ -1,5 +1,8 @@
+import { MongoClient } from "mongodb";
+
 export class MongoManager {
   public static instance: MongoManager;
+  private client: MongoClient | null = null;
   private constructor() {}
 
   public static getInstance(): MongoManager {
@@ -7,5 +10,10 @@ export class MongoManager {
       MongoManager.instance = new MongoManager();
     }
     return MongoManager.instance;
+  }
+  public async connect(url: string): Promise<void> {
+    if (!this.client) {
+      this.client = await MongoClient.connect(url);
+    }
   }
 }
