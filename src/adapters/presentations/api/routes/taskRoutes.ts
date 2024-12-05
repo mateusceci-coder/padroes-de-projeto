61fa5447ec65f261b5440fe0f34381673064fc01
Mateus Ceci <mateusceci@gmail.com>
2024-12-05 08:29:27 -0300

    feat: add adapters to route

--- src/adapters/presentations/api/routes/taskRoutes.ts
+++ src/adapters/presentations/api/routes/taskRoutes.ts
@@ -1,11 +1,10 @@
 import { Request, Response, Router } from "express";
 import { AddTaskController } from "../../../controllers/task/addTask";
+import { expressRouterAdapter } from "../../../expressRouterAdapter";
+import { DateValidatorAdapter } from "../../../dateValidatorAdapter";
 
 export default (router: Router): void => {
-  const addTaskController = new AddTaskController();
-  router.post(
-    "/tasks",
-    async (req: Request, res: Response) =>
-      await addTaskController.handle(req, res)
-  );
+  const dateValidatorAdapter = new DateValidatorAdapter();
+  const addTaskController = new AddTaskController(dateValidatorAdapter);
+  router.post("/tasks", expressRouterAdapter(addTaskController));
 };
