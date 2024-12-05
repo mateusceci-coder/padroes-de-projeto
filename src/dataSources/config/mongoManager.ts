30af5f2842aa293ede661309bf77c0b1a11b460c
Mateus Ceci <mateusceci@gmail.com>
2024-12-05 08:29:40 -0300

    feat: create mongodb instance

---
+++ src/dataSources/config/mongoManager.ts
@@ -0,0 +1,29 @@
+import { Collection, MongoClient } from "mongodb";
+
+export class MongoManager {
+  public static instance: MongoManager;
+  private client: MongoClient | null = null;
+  private constructor() {}
+
+  public static getInstance(): MongoManager {
+    if (!MongoManager.instance) {
+      MongoManager.instance = new MongoManager();
+    }
+
+    return MongoManager.instance;
+  }
+
+  public async connect(url: string) {
+    if (!this.client) {
+      this.client = await MongoClient.connect(url);
+    }
+  }
+
+  public getCollection(name: string): Collection {
+    if (!this.client) {
+      throw new Error("Client is not connected");
+    }
+
+    return this.client?.db().collection(name);
+  }
+}
