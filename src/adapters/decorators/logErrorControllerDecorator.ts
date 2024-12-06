70380b59f907baa4013ec4fc8cb10433efcec90d
Mateus Ceci <mateusceci@gmail.com>
2024-12-06 08:53:36 -0300

    feat: create log error controller decorator

---
+++ src/adapters/decorators/logErrorControllerDecorator.ts
@@ -0,0 +1,16 @@
+import { LogErrorRepository } from "../../usecases/repository/logErrorRepository";
+import { Controller } from "../interfaces/controller";
+
+export class LogErrorControllerDecorator implements Controller {
+  constructor(
+    private readonly controller: Controller,
+    private readonly logErrorRepository: LogErrorRepository
+  ) {}
+  async handle(httpRequest: any): Promise<any> {
+    const httpResponse = await this.controller.handle(httpRequest);
+    if (httpResponse.statusCode === 500) {
+      await this.logErrorRepository.log(httpResponse.body.stack);
+    }
+    return httpResponse;
+  }
+}
