1fe1eee8bb21706a7465e4abfef02e9212e10fb2
Mateus Ceci <mateusceci@gmail.com>
2024-12-06 08:55:33 -0300

    feat: add factory in express router

--- src/adapters/presentations/api/routes/taskRoutes.ts
+++ src/adapters/presentations/api/routes/taskRoutes.ts
@@ -1,10 +1,7 @@
-import { Request, Response, Router } from "express";
-import { AddTaskController } from "../../../controllers/task/addTask";
+import { Router } from "express";
 import { expressRouterAdapter } from "../../../expressRouterAdapter";
-import { DateValidatorAdapter } from "../../../dateValidatorAdapter";
+import { taskControllerFactory } from "../../../factories/taskControllerFactory";
 
 export default (router: Router): void => {
-  const dateValidatorAdapter = new DateValidatorAdapter();
-  const addTaskController = new AddTaskController(dateValidatorAdapter);
-  router.post("/tasks", expressRouterAdapter(addTaskController));
+  router.post("/tasks", expressRouterAdapter(taskControllerFactory()));
 };
