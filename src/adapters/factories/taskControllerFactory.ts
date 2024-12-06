96dff4bcd2da0917314c3df79c8b2cc4d6fb93cb
Mateus Ceci <mateusceci@gmail.com>
2024-12-06 08:54:33 -0300

    feat: create task controller factory

---
+++ src/adapters/factories/taskControllerFactory.ts
@@ -0,0 +1,21 @@
+import { DbAddTask } from "../../dataSources/db/dbAddTask";
+import { LogErrorMongoRepository } from "../../dataSources/db/repository/logErrorMongoRepository";
+import { TaskMongoRepository } from "../../dataSources/db/repository/taskMongoRepository";
+import { AddTaskController } from "../controllers/task/addTask";
+import { LogErrorControllerDecorator } from "../decorators/logErrorControllerDecorator";
+import { addTaskValidationCompositeFactory } from "./addTaskValidationCompositeFactory";
+
+export const taskControllerFactory = () => {
+  const taskMongoRepository = new TaskMongoRepository();
+  const dbAddTask = new DbAddTask(taskMongoRepository);
+  const taskController = new AddTaskController(
+    dbAddTask,
+    addTaskValidationCompositeFactory()
+  );
+  const logErrorMongoRepository = new LogErrorMongoRepository();
+
+  return new LogErrorControllerDecorator(
+    taskController,
+    logErrorMongoRepository
+  );
+};
