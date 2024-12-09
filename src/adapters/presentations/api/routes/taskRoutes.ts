ceca0af5dc75e3da72bdb41c7bad7bb39fddca8c
Mateus Ceci <mateusceci@gmail.com>
2024-12-09 09:17:35 -0300

    feat: add delete route

--- src/adapters/presentations/api/routes/taskRoutes.ts
+++ src/adapters/presentations/api/routes/taskRoutes.ts
@@ -1,7 +1,12 @@
 import { Router } from "express";
 import { expressRouterAdapter } from "../../../expressRouterAdapter";
 import { taskControllerFactory } from "../../../factories/taskControllerFactory";
+import { deleteTaskControllerFactory } from "../../../factories/deleteTaskControllerFactory";
 
 export default (router: Router): void => {
   router.post("/tasks", expressRouterAdapter(taskControllerFactory()));
+  router.delete(
+    "/tasks/:id",
+    expressRouterAdapter(deleteTaskControllerFactory())
+  );
 };
