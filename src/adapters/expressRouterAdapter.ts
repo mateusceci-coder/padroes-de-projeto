e8ca548163e87d30df9091a4e7ba36ac91b19e45
Mateus Ceci <mateusceci@gmail.com>
2024-12-09 09:16:14 -0300

    feat: add params to router adapter request

--- src/adapters/expressRouterAdapter.ts
+++ src/adapters/expressRouterAdapter.ts
@@ -5,11 +5,12 @@ export const expressRouterAdapter = (controller: Controller) => {
   return async (req: Request, res: Response) => {
     const httpRequest = {
       body: req.body,
+      params: req.params,
     };
 
     const httpResponse = await controller.handle(httpRequest);
 
-    if (httpResponse.statusCode === 201) {
+    if (httpResponse.statusCode === 201 || httpResponse.statusCode === 204) {
       res.status(httpResponse.statusCode).json(httpResponse.body);
     } else {
       res.status(httpResponse.statusCode).json({
