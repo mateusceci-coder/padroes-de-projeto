9d5153bb566dc0f89cb81a1543498128488a0333
Mateus Ceci <mateusceci@gmail.com>
2024-12-09 09:18:18 -0300

    feat: add delete method to task mongo repository

--- src/dataSources/db/repository/taskMongoRepository.ts
+++ src/dataSources/db/repository/taskMongoRepository.ts
@@ -1,9 +1,15 @@
+import { ObjectId } from "mongodb";
 import { Task } from "../../../entities/task";
 import { AddATaskModel } from "../../../usecases/addTask";
+import { DeleteTaskModel } from "../../../usecases/deleteTask";
 import { AddTaskRepository } from "../../../usecases/repository/addTaskRepository";
+import { DeleteTaskRepository } from "../../../usecases/repository/deleteTaskRepository";
 import { MongoManager } from "../../config/mongoManager";
+import { InvalidParamError } from "../../../adapters/presentations/api/errors/invalid-param-error";
 
-export class TaskMongoRepository implements AddTaskRepository {
+export class TaskMongoRepository
+  implements AddTaskRepository, DeleteTaskRepository
+{
   async add(taskData: AddATaskModel): Promise<Task> {
     const taskCollection = MongoManager.getInstance().getCollection("tasks");
     const { insertedId } = await taskCollection.insertOne(taskData);
@@ -20,5 +26,20 @@ export class TaskMongoRepository implements AddTaskRepository {
     };
 
     return task;
+  }
+
+  async delete(taskData: DeleteTaskModel): Promise<Error | void> {
+    const taskCollection = MongoManager.getInstance().getCollection("tasks");
+    if (!ObjectId.isValid(taskData.id)) {
+      return new InvalidParamError(taskData.id);
+    }
+
+    const { deletedCount } = await taskCollection.deleteOne({
+      _id: new ObjectId(taskData.id),
+    });
+
+    if (!deletedCount) {
+      return new Error("Task not found");
+    }
   }
 }
